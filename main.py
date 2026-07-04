a837b9556d04843258dac00d0064e4d2a7bf9041
amn004798-source <amn004798@gmail.com>
2026-07-04 18:30:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import asyncio
+asyncio.set_event_loop(asyncio.new_event_loop())
+
 from pyrogram import Client
 from config.settings import API_ID, API_HASH, BOT_TOKEN
-import plugins  # this imports all plugins (start, upload, share, myfiles, admin)
 
-app = Client("mars_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client(
+    "mars_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins")
+)
 
 if __name__ == "__main__":
     app.run()
